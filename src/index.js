4ad2b46067b42a7834817e98473082ec4e584a68
R1Z0bug <letientlu@gmail.com>
2022-04-05 13:44:16 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const path = require('path')
-//const morgan = require('morgan');
+const morgan = require('morgan');
 const hbs  = require('express-handlebars');
 
 // import { engine } from 'express-handlebars';
 const app = express()
-const port = 3000
+const port = 2509
 
 app.use(express.static(path.join(__dirname,'pulic')))
 
 
 //http logger
-//app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 
 //template engine
-app.engine('handlebars', hbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
+
+app.set('view engine', '.hbs');
 //app.set('views', __dirname+'\\resources\\views'); c1
+
 app.set('views',path.join(__dirname, 'resources', 'views')); //c2
 
 
 
 app.get('/', (req, res, next) => {
-  res.render('home', {layout: false});
+  res.render('home');
+});
+
+
+app.get('/news', (req, res, next) => {
+  res.render('news');
+});
+
+app.get('/search',(req, res, next) => {
+  res.render('search', {layout: false});
 });
 
 app.listen(port, () => {
